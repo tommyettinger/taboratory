b9cf47d37437b13c61b1eb0f68d2389586d7e9bd
Tommy Ettinger <tommy.ettinger@gmail.com>
2025-07-03 21:14:58 -0700

    Bring in PicoCLI structure from SpotVox...

---
+++ headless/src/main/java/com/github/tommyettinger/headless/HeadlessLauncher.java
@@ -0,0 +1,51 @@
+package com.github.tommyettinger.headless;
+
+import com.badlogic.gdx.backends.headless.HeadlessApplication;
+import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
+import com.github.tommyettinger.Taboratory;
+import picocli.CommandLine;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(name = "taboratory", version = "Taboratory 0.0.1-SNAPSHOT",
+		description = "Takes a .tsv tab-separated value file and writes Java sources inside './generated/' .",
+		mixinStandardHelpOptions = true)
+public class HeadlessLauncher implements Callable<Integer> {
+
+	@CommandLine.Parameters(description = "The absolute or relative path to one or more tab-separated value (.tsv) files.", defaultValue = "*.tsv")
+	public String input = "*.tsv";
+
+	public static void main(String[] args) {
+		int exitCode = new picocli.CommandLine(new HeadlessLauncher()).execute(args);
+		System.exit(exitCode);
+	}
+
+	@Override
+	public Integer call() {
+		HeadlessApplicationConfiguration configuration = new HeadlessApplicationConfiguration();
+		configuration.updatesPerSecond = -1;
+		try {
+			//// loads a file by its full path, which we get via a command-line arg
+			FileInputStream received = new FileInputStream(input);
+
+			int nameStart = Math.max(input.lastIndexOf('/'), input.lastIndexOf('\\')) + 1;
+			this.input = input.substring(nameStart, input.indexOf('.', nameStart));
+			new HeadlessApplication(new Taboratory(input, configuration){
+				{
+					try {
+						mainLoopThread.join();
+					} catch (InterruptedException e) {
+						System.out.println("Interrupted!");
+					}
+				}
+			};
+
+		} catch (Exception e) {
+			System.out.println("Parameters are not valid. Run with -h to show help.");
+			return -1;
+		}
+		return 0;
+	}
+}
