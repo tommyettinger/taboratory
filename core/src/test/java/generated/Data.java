3143e0ef268499d520a1e64be02ecc788487b827
Tommy Ettinger <tommy.ettinger@gmail.com>
2025-08-29 22:53:31 -0700

    OK, this is how it should look when we're done...

--- core/src/test/java/generated/Data.java
+++ core/src/test/java/generated/Data.java
@@ -1,10 +1,13 @@
 package generated;
 
+import com.github.tommyettinger.digital.Base;
+import com.github.tommyettinger.digital.Hasher;
 import com.github.tommyettinger.digital.TextTools;
 import com.github.tommyettinger.ds.Junction;
 import com.github.tommyettinger.ds.ObjectIntOrderedMap;
 import com.github.tommyettinger.ds.ObjectList;
 import com.github.tommyettinger.ds.ObjectObjectOrderedMap;
+import com.github.tommyettinger.ds.support.util.PartialParser;
 
 import java.util.Arrays;
 import java.util.List;
@@ -59,21 +62,45 @@ public class Data {
     this.__code = __code;
   }
 
+    /**
+     * Not actually generated code at the moment!
+     * @param line a single line of tab-separated fields to be parsed by fixed logic generated at class generation
+     * @return a new Data item parsed from line
+     */
+  public static Data parse(String line) {
+      String[] split = TextTools.split(line, "\t");
+      String p0 = split[0];
+      String p1 = split[1];
+      int p2 = Base.BASE10.readInt(split[2]);
+      ObjectIntOrderedMap<Junction<String>> p3 = new ObjectIntOrderedMap<>();
+      p3.putLegible(split[3], ", ", ", ", PartialParser.DEFAULT_JUNCTION_STRING);
+      String p4 = split[4];
+      Junction<String> p5 = Junction.parse(split[5]);
+      ObjectList<String> p6 = new ObjectList<>();
+      p6.addLegible(split[6], ", ", PartialParser.DEFAULT_STRING);
+      ObjectList<String> p7 = new ObjectList<>();
+      p6.addLegible(split[7], ", ", PartialParser.DEFAULT_STRING);
+      Junction<String> p8 = Junction.parse(split[8]);
+      String p9 = split[9];
+      long hash = Hasher.tau.hashBulk64(line);
+      return new Data(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, hash);
+  }
+
     public static ObjectObjectOrderedMap<String, Data> parseAll(List<String> lines) {
         if (lines == null || lines.isEmpty()) return new ObjectObjectOrderedMap<>();
 
-        String[] header = TextTools.split(lines.get(0), "\t");
+        String firstLine = lines.get(0);
+        String[] header = TextTools.split(firstLine, "\t");
         if (!Arrays.deepEquals(__headerLine, header))
-            throw new IllegalArgumentException("Header lines do not match: expected " +
-                    TextTools.join("\t", __headerLine) + ", got " + TextTools.join("\t", header));
+            throw new IllegalArgumentException("Header lines do not match! Expected:\n" +
+                    TextTools.join("\t", __headerLine) + "\nbut got:\n" + firstLine);
 
         int numLines = lines.size();
         ObjectObjectOrderedMap<String, Data> all = new ObjectObjectOrderedMap<>(numLines);
         for (int i = 1; i < numLines; i++) {
             String current = lines.get(i);
-            String[] split = TextTools.split(current, "\t");
-            //now we need generate the bigger code to parse each constructor parameter according to header fields...
-
+            Data parsed = parse(current);
+            all.put(parsed.key(), parsed);
         }
         return all;
     }
