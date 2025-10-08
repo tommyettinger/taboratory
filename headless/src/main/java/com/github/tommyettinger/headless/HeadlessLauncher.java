a6f93ee5ef6f0995707156d040e9eab6d82c3ec1
Tommy Ettinger <tommy.ettinger@gmail.com>
2025-10-07 20:38:17 -0700

    Update jdkgdxds, add JB Annotations (maybe).
    There's slightly better error message printing now; it won't swallow the whole stacktrace...

--- headless/src/main/java/com/github/tommyettinger/headless/HeadlessLauncher.java
+++ headless/src/main/java/com/github/tommyettinger/headless/HeadlessLauncher.java
@@ -6,28 +6,29 @@ import picocli.CommandLine;
 import java.util.concurrent.Callable;
 
 @CommandLine.Command(name = "taboratory", version = "Taboratory 0.0.2",
-		description = "Takes a .tsv tab-separated value file and writes Java sources inside './generated/' .",
-		mixinStandardHelpOptions = true)
+        description = "Takes a .tsv tab-separated value file and writes Java sources inside './generated/' .",
+        mixinStandardHelpOptions = true)
 public class HeadlessLauncher implements Callable<Integer> {
 
-	@CommandLine.Parameters(description = "The absolute or relative path to one or more tab-separated value (.tsv) files.", defaultValue = "Data.tsv")
-	public String input = "Data.tsv";
+    @CommandLine.Parameters(description = "The absolute or relative path to one or more tab-separated value (.tsv) files.", defaultValue = "Data.tsv")
+    public String input = "Data.tsv";
 
-	public static void main(String[] args) {
-		int exitCode = new picocli.CommandLine(new HeadlessLauncher()).execute(args);
-		System.exit(exitCode);
-	}
+    public static void main(String[] args) {
+        int exitCode = new picocli.CommandLine(new HeadlessLauncher()).execute(args);
+        System.exit(exitCode);
+    }
 
-	@Override
-	public Integer call() {
-		try {
-			// creates a file in the project root, next to settings.gradle
-			Taboratory.run(input);
-		} catch (Exception e) {
-			System.out.println("Parameters are not valid, or something failed. Run with -h to show help.\n" +
+    @Override
+    public Integer call() {
+        try {
+            // creates a file in the project root, next to settings.gradle
+            Taboratory.run(input);
+        } catch (Exception e) {
+            System.out.println("Parameters are not valid, or something failed. Run with -h to show help.\n" +
                     "Original error message:\n" + e.getLocalizedMessage());
-			return -1;
-		}
-		return 0;
-	}
+            e.printStackTrace();
+            return -1;
+        }
+        return 0;
+    }
 }
